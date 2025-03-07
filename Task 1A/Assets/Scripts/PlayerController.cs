f37e586919e6fbe28fdcc6bf09b0ea2505436126
NedTanner <ert015@student.usc.edu.au>
2025-03-07 10:30:01 +1000

    7/03/25 Update
    - Deleted unnecessary camera script
    - Deleted unused scripts and assets
    - Changed player to a cube
    - Cleaned up formatting in PlayerMovement script
    - Renamed variables to fit Unity naming conventions
    - Reorganised objects in scene
    - Changed win screen to an image (WIP)

---
+++ Task 1A/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+	public float playerSpeed;
+
+	public Text pickupText;
+	public GameObject winScreen;
+
+	private Rigidbody rb;
+	private float pickupCounter;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+		pickupCounter = 0;
+		SetCountText();
+	}
+	// Creates local movement variable controlled by horizontal and vertical inputs, applies force to player object.
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+		rb.AddForce(movement * playerSpeed);
+	}
+
+	// Check if collision is a pickup, updates counter text.
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag("Pick Up"))
+		{
+			other.gameObject.SetActive(false);
+			pickupCounter += 1;
+			SetCountText();
+		}
+	}
+	// Updates counter text and checks if win state is active.
+	void SetCountText()
+	{
+		pickupText.text = "Cubes Collected: " + pickupCounter.ToString();
+		if (pickupCounter >= 12)
+		{
+			winScreen.SetActive(true);
+		}
+	}
+}
